51358df548d4e85d472ef0392d3ba82b1e204bd3
misalibaytb <66683443+misalibaytb@users.noreply.github.com>
2025-03-20 21:32:13 +0100

    Refactor login and registration forms to remove firstname and surname fields from login, while adding them back to registration for improved user experience.

--- frontend/src/newApp.tsx
+++ frontend/src/newApp.tsx
@@ -300,8 +300,6 @@ export const Products = () => {
 
 export const Login = () => {
     const navigate = useNavigate()
-    const [firstname, setFirstname] = useState("")
-    const [surname, setSurname] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState("")
@@ -313,7 +311,7 @@ export const Login = () => {
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ firstname, surname, email, password }),
+            body: JSON.stringify({ email, password }),
             credentials: "include"
         }).then(async (res) => {
             if (res.ok) {
@@ -340,15 +338,6 @@ export const Login = () => {
         <div className="container">
             <form onSubmit={e => { e.preventDefault(); login() }}>
                 <h1>Přihlášení</h1>
-                <div className="form-group">
-                    <label htmlFor="email">Jméno:</label>
-                    <input type="text" id="firstname" value={email} onChange={e => setFirstname(e.target.value)} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">Příjmení:</label>
-                    <input type="text" id="surname" value={email} onChange={e => setSurname(e.target.value)} />
-                </div>
-
                 <div className="form-group">
                     <label htmlFor="email">Email:</label>
                     <input type="email" id="email" value={email} onChange={e => setEmail(e.target.value)} />
@@ -417,6 +406,8 @@ export const Login = () => {
 
 export const Register = () => {
     const navigate = useNavigate()
+    const [firstname, setFirstname] = useState("")
+    const [surname, setSurname] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState("")
@@ -428,7 +419,7 @@ export const Register = () => {
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({ firstname, surname, email, password }),
             credentials: "include"
         }).then(async (res) => {
             if (res.ok) {
@@ -455,6 +446,14 @@ export const Register = () => {
         <div className="container">
             <form onSubmit={e => { e.preventDefault(); login() }}>
                 <h1>Registrace</h1>
+                <div className="form-group">
+                    <label htmlFor="email">Jméno:</label>
+                    <input type="text" id="firstname" value={firstname} onChange={e => setFirstname(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Příjmení:</label>
+                    <input type="text" id="surname" value={surname} onChange={e => setSurname(e.target.value)} />
+                </div>
                 <div className="form-group">
                     <label htmlFor="email">Email:</label>
                     <input type="email" id="email" value={email} onChange={e => setEmail(e.target.value)} />
